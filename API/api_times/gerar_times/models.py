1131b8401a3853278e517e1ee5b8a72a830518e6
Cesar Block <cesar.2022@alunos.utfpr.edu.br>
2023-11-12 13:39:04 -0300

    Models

--- API/api_times/gerar_times/models.py
+++ API/api_times/gerar_times/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
-#python manage.py makemigrations gerar_times PREPARA O MIGRATIONS
-#python manage.py migrate EXECUTA O MIGRATIONS
+
+# python manage.py makemigrations gerar_times PREPARA O MIGRATIONS
+# python manage.py migrate EXECUTA O MIGRATIONS
 
 class Aluno(models.Model):
+    Id = models.AutoField(primary_key=True)
     Nome = models.CharField(max_length=150)
     Area_de_atuacao = models.CharField(max_length=30)
     Nivel_de_senioridade = models.CharField(max_length=10)
@@ -15,10 +17,45 @@ class Aluno(models.Model):
     class Meta:
         db_table = 'Aluno'
 
+
 class Professor(models.Model):
+    Id = models.AutoField(primary_key=True)
     Email = models.CharField(max_length=50, unique=True)
     Senha = models.CharField(max_length=50)
     Nome = models.CharField(max_length=150)
 
     class Meta:
         db_table = 'Professor'
+
+
+class Sala(models.Model):
+    Id = models.AutoField(primary_key=True)
+    Nome = models.CharField(max_lenght=100)
+    fk_Professor_Id = models.ForeignKey(Professor, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'salas'
+
+
+class Times(models.Model):
+    Id = models.AutoField(primary_key=True)
+    fk_Salas = models.ForeignKey(Sala, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'times'
+
+
+# class Participa(models.Model):
+#     fk_Salas_Id = models.ForeignKey(Sala, on_delete=models.RESTRICT)
+#     fk_Aluno_Id = models.ForeignKey(Aluno, on_delete=models.SETNULL)
+#
+#     class Meta:
+#         db_table = 'participa'
+
+
+class Contem(models.Model):
+    fk_Aluno_Id = models.ForeignKey(Aluno, on_delete=models.RESTRICT)
+    fk_Times_Id = models.ForeingKey(Times, on_delete=models.SETNULL)
+
+    class Meta:
+        db_table = 'contem'
